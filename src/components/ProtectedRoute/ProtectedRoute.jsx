1a40447ef10883c8186c3105b2295e84c1d25599
Babitha <babithababi340@gmail.com>
2025-03-13 09:54:10 -0600

    Work on media screen

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -9,7 +9,6 @@ const ProtectedRoute = ({ isLoggedIn, children }) => {
   const navigate = useNavigate();
   useEffect(() => {
     if (!currentUser || !isLoggedIn) {
-      debugger;
       navigate("/");
     }
   }, [currentUser, isLoggedIn]);
